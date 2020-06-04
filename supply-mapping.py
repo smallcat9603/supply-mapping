20136a877c69efc2762c90f3962df8ea1b649892
smallcat9603 <huyao0107@gmail.com>
2020-06-05 00:29:55 +0900

    minor

--- supply-mapping.py
+++ supply-mapping.py
@@ -9,23 +9,21 @@ import networkx as nx
 import sys, getopt
 import pandas as pd
 
-#工場数
+#number of plants
 num_location = 4
-#製品数
+#number of products
 num_product = 6
 
-#製品-工場のマッピング、0は工場A、1は工場B、...
+#product-plant mapping
 supply_mapping = [0] * num_product
-#コスト最適化後の結果
-location_bestfit = {}
 
-#製品間の依存関係
+#product relation
 product_parts = {
     6:[3, 4, 5],
     3:[1, 2]
 }
 
-#時間コスト
+#time cost
 time_cost = {
     1:[1, 2, 3, 4],
     2:[1, 2, 3, 4],
@@ -35,7 +33,7 @@ time_cost = {
     6:[1, 2, 3, 4]
 }
 
-#コスト
+#dollar cost
 dollar_cost = {
     1:[1, 2, 3, 4],
     2:[1, 2, 3, 4],
@@ -45,7 +43,7 @@ dollar_cost = {
     6:[1, 2, 3, 4]    
 }
 
-#製品の工場効率
+#product efficiency
 efficiency = {
     1:[1, 2, 3, 4],
     2:[1, 2, 3, 4],
@@ -55,7 +53,7 @@ efficiency = {
     6:[1, 2, 3, 4]       
 }
 
-#工場間の物理距離
+#plant distance
 physical_distance = [
     [0, 1, 2, 3],
     [1, 0, 3, 4],
@@ -63,7 +61,7 @@ physical_distance = [
     [3, 4, 5, 0]
 ]
 
-#製品間の調整関係
+#adjust relation of products
 adjust_relation = [
     [1, 1, 0, 1, 0, 1],
     [1, 1, 1, 0, 1, 1],
@@ -73,7 +71,7 @@ adjust_relation = [
     [1, 1, 1, 1, 1, 1]
 ]
 
-#工場間の社会関係
+#social relation of plants
 social_relation = [
     [0, 1, 2, 3],
     [1, 0, 3, 4],
@@ -81,45 +79,60 @@ social_relation = [
     [3, 4, 5, 0]    
 ]
 
-#工場の調整能力
+#adjust capability of plants
 location_capability = [1, 2, 3, 4]
 
-#製品の需要個数
+#demands
 product_demand = [1, 2, 3, 4] 
 
 
 def main():
-    for demand in product_demand:
-        if demand > 0:
-            for factory in range(num_location):
-                supply_mapping[num_product-1] = factory
-                cost_min = 10000
-                cost_production_min = 0
-                cost_transportation_min = 0
-                cost_adjust_min = 0
-                # for b in range(pow(2, num_product-1)):        
-                #     for product in range(num_product-1):
-                #         supply_mapping[product] = (bin(b)>>product)&1
-                for p0 in range(num_location):
-                    for p1 in range(num_location):
-                        for p2 in range(num_location):
-                            for p3 in range(num_location):
-                                for p4 in range(num_location):
-                                    supply_mapping[0] = p0
-                                    supply_mapping[1] = p1
-                                    supply_mapping[2] = p2
-                                    supply_mapping[3] = p3
-                                    supply_mapping[4] = p4
-                                    cost_production = cal_cost_production()
-                                    cost_transportation = cal_cost_transportation()
-                                    cost_adjust = cal_cost_adjust()
-                                    cost = cost_production + cost_transportation + cost_adjust
-                                    if cost < cost_min:
-                                        cost_min = cost
-                                        cost_production_min = cost_production
-                                        cost_transportation_min = cost_transportation
-                                        cost_adjust_min = cost_adjust
-                                    print_result(factory, cost_min, cost_production_min, cost_transportation_min, cost_adjust_min)
+    print "Number of plants: ", num_location
+    print "Number of products: ", num_product
+    print "Optimization result: " 
+    for factory in range(num_location):
+        if product_demand[factory] > 0:
+            supply_mapping[num_product-1] = factory
+            cost_min = 10000
+            cost_production_min = 0
+            cost_transportation_min = 0
+            cost_adjust_min = 0
+            cost_min_2 = 10000
+            cost_production_min_2 = 0
+            cost_transportation_min_2 = 0
+            cost_adjust_min_2 = 0            
+            # for b in range(pow(2, num_product-1)):        
+            #     for product in range(num_product-1):
+            #         supply_mapping[product] = (bin(b)>>product)&1
+            for p0 in range(num_location):
+                for p1 in range(num_location):
+                    for p2 in range(num_location):
+                        for p3 in range(num_location):
+                            for p4 in range(num_location):
+                                supply_mapping[0] = p0
+                                supply_mapping[1] = p1
+                                supply_mapping[2] = p2
+                                supply_mapping[3] = p3
+                                supply_mapping[4] = p4
+                                cost_production = cal_cost_production()
+                                cost_transportation = cal_cost_transportation()
+                                cost_adjust = cal_cost_adjust()
+                                cost = cost_production + cost_transportation + cost_adjust
+                                cost_2 = cost_production + cost_transportation
+                                if cost < cost_min:
+                                    cost_min = cost
+                                    cost_production_min = cost_production
+                                    cost_transportation_min = cost_transportation
+                                    cost_adjust_min = cost_adjust  
+                                if cost_2 < cost_min_2:
+                                    cost_min_2 = cost_2
+                                    cost_production_min_2 = cost_production
+                                    cost_transportation_min_2 = cost_transportation
+                                    cost_adjust_min_2 = cost_adjust
+            print_result(factory, cost_min, cost_production_min, cost_transportation_min, cost_adjust_min)
+            print_mapping()
+            print_result_2(factory, cost_min_2, cost_production_min_2, cost_transportation_min_2, cost_adjust_min_2)
+            print_mapping()
 
 def cal_cost_production():
     cost_production = 0
@@ -139,7 +152,7 @@ def cal_cost_transportation():
 
 def cal_cost_adjust():
     cost_adjust = 0
-    for p in adjust_relation:
+    for p in range(num_product):
         for pp in range(p, num_product):
             if adjust_relation[p][pp] == 1:
                 location_p = supply_mapping[p]
@@ -147,10 +160,18 @@ def cal_cost_adjust():
                 cost_adjust = cost_adjust + pow(social_relation[location_p][location_pp], 2)/(location_capability[location_p]*location_capability[location_pp])
     return cost_adjust
 
-def print_result():
-    print "工場", factory, ": \n"
-    print "総コスト: ", cost_min, " (生産コスト: ", cost_production_min, ", 物流コスト: ", cost_transportation_min, ", 調整コスト: ", cost_adjust_min, ") \n"
+def print_result(factory, cost_min, cost_production_min, cost_transportation_min, cost_adjust_min):
+    print "Plant", factory, ": "
+    print "Total cost (3 considered): ", cost_min, " (production cost: ", cost_production_min, ", transportation cost: ", cost_transportation_min, ", adjust cost: ", cost_adjust_min, ")"
 
+def print_result_2(factory, cost_min_2, cost_production_min_2, cost_transportation_min_2, cost_adjust_min_2):
+    print "Plant", factory, ": "
+    print "Total cost (2 considered): ", cost_min_2, " (production cost: ", cost_production_min_2, ", transportation cost: ", cost_transportation_min_2, ", adjust cost: ", cost_adjust_min_2, ")"
+
+def print_mapping():
+    print "product --> plant:"
+    for product in range(num_product):
+        print product+1, " --> ", supply_mapping[product]
 
 if __name__ == "__main__":
    main()      
